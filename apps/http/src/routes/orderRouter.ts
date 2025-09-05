300284dfcd063af5cf4d0791caac5d042143cf43
Gaurav Kapur <gauravkapur596@gmail.com>
2025-09-05 16:17:02 +0530

    added orderRouter

--- apps/http/src/routes/orderRouter.ts
+++ apps/http/src/routes/orderRouter.ts
@@ -1,7 +1,21 @@
 import { Router } from "express";
+import { RedisManager } from "../RedisManager";
 
+export const CREATE_ORDER = "CREATE_ORDER";
 export const orderRouter: Router = Router();
 
-orderRouter.post("/", async(req,res) => {
-    const {market,price,quanitiy,side,userId} = req.body;
-} )
+orderRouter.post("/", async (req, res) => {
+    const { market, price, quantity, side, userId } = req.body;
+    console.log({ market, price, quantity, side, userId })
+    const response = await RedisManager.getInstance().sendAndAwait({
+        type: CREATE_ORDER,
+        data: {
+            market,
+            price,
+            quantity,
+            side,
+            userId
+        }
+    });
+    res.json(response.payload);
+});
